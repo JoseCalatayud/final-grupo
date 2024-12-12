2be2d36f7bb2c485533874af1e2bf270e78f6a01
curso05 <notiene@notiene.es>
2024-12-12 13:27:14 +0100

    evaluar Numero y comprobar Intentos

--- back/src/main/java/es/ascender/proyectogrupo/Juego.java
+++ back/src/main/java/es/ascender/proyectogrupo/Juego.java
@@ -1,38 +1,41 @@
 package es.ascender.proyectogrupo;
 
-
-
 public class Juego {
 
     private int numeroAleatorio;
     private int intentosMax;
     private Jugador jugador;
 
-
     public Juego(int intentosMax, String nombre) {
-        this.numeroAleatorio = (int) (Math.random()*10);
+        this.numeroAleatorio = (int) (Math.random() * 10);
         this.intentosMax = intentosMax;
         this.jugador = new Jugador(nombre);
 
     }
-    
 
-    public String comprobarNumero (int numero){
-        int intentosJugador = jugador.getIntentos() + 1;
-        jugador.setIntentos(intentosJugador);
-        if(jugador.getIntentos()>intentosMax)
-        if(numeroAleatorio>numero){            
-            return "El numero es mayor";
-        }else if(numeroAleatorio<numero){
-            return "El numero es menor";        }
-        return "Enhorabuena, has acertado";
-
+    public Jugador getJugador() {
+        return jugador;
     }
 
+    public String comprobarIntento(int numero) {
+        int intentosJugador = jugador.getIntentos() + 1;
+        jugador.setIntentos(intentosJugador);
+        if (intentosJugador <= intentosMax) {
+            return evaluarNumero(numero);
+        } else {
+            return "Intentos Superados";
+        }
+    }
 
-
-
-    
-    
+    public String evaluarNumero(int numero) {
+        String respuesta;
+        if (numeroAleatorio > numero) {
+            respuesta = "El numero es mayor";
+        } else if (numeroAleatorio < numero) {
+            respuesta = "El numero es menor";
+        }
+        respuesta = "Enhorabuena, has acertado";
+        return respuesta;
+    }
 
 }
