f1e1bdd826c0e6e6981427b852c458bafed0e6bd
curso12 <curso12@curso12>
2024-12-13 12:39:29 +0100

    Revert "Add second part of the tests"
    This reverts commit 6ef0942c05e083b18c1c157b2aa11788566d8f0d.

--- back/src/test/java/es/ascender/proyectogrupo/JuegoTest.java
+++ back/src/test/java/es/ascender/proyectogrupo/JuegoTest.java
@@ -32,38 +32,6 @@ public class JuegoTest {
         
     }
 
-    @Test
-    void testComprobarIntentoMayor() {
-        int numero = juego.numeroAleatorio +1;
-        
-        String resultado = juego.comprobarIntento(numero);
-        assertEquals("El numero es mayor", resultado);
-    }
-
-    @Test 
-    void testComprobarIntentoMenor() {
-        int numero = juego.numeroAleatorio -1;
-
-        String resultado = juego.comprobarIntento(numero);
-        assertEquals("El numero es menor", resultado);
-    }
-
-    @Test
-    void testIntentoExeededAttempts() {
-        for (int i = 0; i<0; i++) {
-            juego.comprobarIntento(-1);
-        }
-        String resultado = juego.comprobarIntento(-1);
-        assertEquals("resultado", resultado);
-    }
-
-   /// @Test
-   // void testEvaluarNumeroAcierto() {
-     //   int numero = juego.numeroAleatorio;
-//
-  //      String resultado = juego.evaluarNumero(numero)
-    //}
-
 
 
     
