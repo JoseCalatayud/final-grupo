29a9a754f3eb3212aa0c9f5f16d19ede3e8adcd3
curso05 <notiene@notiene.es>
2024-12-13 14:07:51 +0100

    back ok

--- back/src/main/java/es/ascender/proyectogrupo/App.java
+++ back/src/main/java/es/ascender/proyectogrupo/App.java
@@ -3,22 +3,27 @@ package es.ascender.proyectogrupo;
 import java.util.Scanner;
 
 public class App {
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido a Adivina el Numero");
         System.out.println("Dime tu nombre");
-        Juego juego = new Juego(10, sc.nextLine());
+        Juego juego = new Juego(6, sc.nextLine());
         System.out.println("Hola " + juego.getJugador().getNombre() + ", tienes que adivinar un numero entre 0 y 10.");
-        System.out.println("Tienes 3 intentos");
-        while (juego.getJugador().getIntentos() <= 3) {
+        System.out.println("Tienes 6 intentos");
+
+
+        while (juego.getJugador().getIntentos() < 6 ) {
             System.out.println("Introduce un numero");
             int numero = sc.nextInt();
             System.out.println(juego.comprobarIntento(numero));
-            ;
+            if (juego.evaluarNumero(numero)==3){
+                break;
+            }
         }
-            
+        sc.close();
+ 
             
 
 
-  */  
-}
+    
+}}
--- back/src/main/java/es/ascender/proyectogrupo/Juego.java
+++ back/src/main/java/es/ascender/proyectogrupo/Juego.java
@@ -10,34 +10,66 @@ public class Juego {
         this.numeroAleatorio = (int) (Math.random() * 10);
         this.intentosMax = intentosMax;
         this.jugador = new Jugador(nombre);
-
     }
 
     public Jugador getJugador() {
         return jugador;
     }
 
-    public String comprobarIntento(int numero) {
-        int intentosJugador = jugador.getIntentos() + 1;
-        jugador.setIntentos(intentosJugador);
-        if (intentosJugador <= intentosMax) {
-            return evaluarNumero(numero);
-        } else {
-            return "Intentos Superados";
+    public boolean evaluarIntentos() {
+
+        if (jugador.getIntentos() < intentosMax) {
+            return true;
         }
+        return false;
     }
 
-    public String evaluarNumero(int numero) {
-        String respuesta="";
-        if (numeroAleatorio > numero) {
-            respuesta = "El numero es mayor";
-        } else if (numeroAleatorio < numero) {
-            respuesta = "El numero es menor";
-        } else if (numeroAleatorio == numero){
-            respuesta = "Has acertado";
+    // public String comprobarIntento(int numero) {
+    // String respuesta = "";
+    // jugador.setIntentos(jugador.getIntentos() + 1);
+    // if (evaluarIntentos() && (evaluarNumero(numero) == 1)) {
+    // respuesta = "Es un numero mayor";
+    // } else if (evaluarIntentos() && (evaluarNumero(numero) == 2)) {
+    // respuesta = "Es un numero menor";
+    // } else if (!evaluarIntentos()) {
+    // respuesta = "Has agotado tus intentos";
+    // } else if (evaluarIntentos() && evaluarNumero(numero) == 3) {
+    // respuesta = "Has acertado!!!";
+    // }
+    // return respuesta;
+    // }
+    public String comprobarIntento(int numero) {
+        int caso = evaluarNumero(numero);
+        String respuesta = "";
+        switch (caso) {
+            case 1:
+                respuesta = "Es un numero mayor. Llevas "+ jugador.getIntentos() + " intentos";
+            case 2:
+                respuesta = "Es un numero menor";
+            case 3:
+                respuesta = "Enhorabuena, has ganado!!!";
+            case 4:
+                respuesta = "Has agotado tus intentos";
+
         }
         return respuesta;
     }
 
-    
+    public int evaluarNumero(int numero) {
+        int respuesta = 0;
+        if (evaluarIntentos()) {
+            if (numeroAleatorio > numero) {
+                respuesta = 1;
+            } else if (numeroAleatorio < numero) {
+                respuesta = 2;
+            } else if (numeroAleatorio == numero) {
+                respuesta = 3;
+            }
+        } else {
+            respuesta = 4;
+        }
+        return respuesta;
+
+    }
+
 }
